1ed0509fa85392eaa0d8c7d94567b8a06d67aeb7
krishpranav <krisna.pranav@gmail.com>
2021-11-28 17:46:38 +0530

    kingkong: utils header

--- include/kingkong/utils.h
+++ include/kingkong/utils.h
@@ -23,6 +23,21 @@ namespace kingkong  {
         
     public:
         template<unsigned N>
+        constexpr const_str(const char (&arr)[N]):
+            begin_(arr), size_(N - 1)
+        {
+            static_assert(N >= 1, "not a string");
+        }
+
+        constexpr char operator[](unsigned i) const
+        {
+            return requires_in_range(i, size_), begin_[i];
+        }
+
+        constexpr operator const char*() const
+        {
+            return begin_;
+        }
     }
 
 #endif
