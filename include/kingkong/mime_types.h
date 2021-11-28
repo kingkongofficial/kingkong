5f81a8030c42977a9df4915d399f45161ff89753
krishpranav <krisna.pranav@gmail.com>
2021-11-28 17:28:43 +0530

    mime types
    different textual values

---
+++ include/kingkong/mime_types.h
@@ -0,0 +1,9 @@
+#include <unordered_map>
+#include <string>
+
+namespace kingkong {
+    const std::unordered_map<std::string, std::string> mine_types {
+        {"html", "text/html"},
+        {"css", "text/css"},
+    };
+}
