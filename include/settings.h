409dff0a2272e79b93b25e8ed9bf8e3eb079d7f8
krishpranav <krisna.pranav@gmail.com>
2021-11-28 15:41:50 +0530

    include: settings
    added comments for explaing the code :0

--- include/settings.h
+++ include/settings.h
@@ -2,8 +2,27 @@
 
 #pragma once
 
+/**
+ * @brief enable logging
+ * 
+ */
 #define KINGKONG_ENABLE_LOG
 
+/**
+ * @brief log levels
+ * 
+ */
 #ifndef KINGKONG_LOG_LEVEL
 #define KINGKONG_LOG_LEVEL 1
 #endif
+
+/**
+ * @brief static dir functions 
+ */
+#ifndef KINGKONG_STATIC_DIR
+#define KONGKONG_STATIC_DIR "static/"
+#endif 
+
+#ifndef KINGKONG_STATIC_ENDPOINT
+#define KONGKONG_STATIC_ENDPOINT "/static/<path>/"
+#endif 
