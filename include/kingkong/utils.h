f8d3f1003defe83ca08ffaca35a9e4ed6758cd51
krishpranav <krisna.pranav@gmail.com>
2021-11-28 17:56:08 +0530

    kingkong: utils

--- include/kingkong/utils.h
+++ include/kingkong/utils.h
@@ -48,6 +48,37 @@ namespace kingkong  {
         }
     };
 
+    constexpr unsigned find_closing_tag(const_str s, unsigned p)
+    {
+        return s[p] == '>' ? p : find_closing_tag(s, p + 1);
+    }
+
+    constexpr bool is_valid(const_str s, unsigned i = 0, int f = 0)
+        {
+            return i == s.size()   ? f == 0 :
+                   f < 0 || f >= 2 ? false :
+                   s[i] == '<'     ? is_valid(s, i + 1, f + 1) :
+                   s[i] == '>'     ? is_valid(s, i + 1, f - 1) :
+                                     is_valid(s, i + 1, f);
+        }
+
+    constexpr bool is_equ_p(const char* a, const char* b, unsigned n)
+        {
+            return *a == 0 && *b == 0 && n == 0 ? true :
+                   (*a == 0 || *b == 0)         ? false :
+                   n == 0                       ? true :
+                   *a != *b                     ? false :
+                                                  is_equ_p(a + 1, b + 1, n - 1);
+        }
+
+    constexpr bool is_equ_n(const_str a, unsigned ai, const_str b, unsigned bi, unsigned n)
+        {
+            return ai + n > a.size() || bi + n > b.size() ? false :
+                   n == 0                                 ? true :
+                   a[ai] != b[bi]                         ? false :
+                                                            is_equ_n(a, ai + 1, b, bi + 1, n - 1);
+        }
+
 #endif
 
     }
