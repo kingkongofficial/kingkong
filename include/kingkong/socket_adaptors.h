e1d5b3e2d4ec25e62a66963afd82e6ac065708ec
krishpranav <krisna.pranav@gmail.com>
2021-11-28 16:43:18 +0530

    kingkong: socket adaptors

--- include/kingkong/socket_adaptors.h
+++ include/kingkong/socket_adaptors.h
@@ -1,3 +1,19 @@
 #pragma once
 
 #include <boost/asio.hpp>
+#ifndef CROW_ENABLE_SSL 
+#include <boost/asio/ssl.h>
+#endif
+
+#include "kingkong/settings.h"
+#if BOOST_VERSION >= 107000
+#define GET_IO_SERVICE(s) ((boost::asio::io_context&)(s).get_executor().context())
+#endif 
+#define GET_IO_SERVICE(s) ((s).get_io_service())
+#endif 
+
+namespace kingkong
+{
+    using namespace boost;
+    
+} 
