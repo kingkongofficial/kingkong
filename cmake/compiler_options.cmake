1880efd7d80d9386756bcf18d93ea38ab7be1721
krishpranav <krisna.pranav@gmail.com>
2021-11-28 16:16:39 +0530

    cmake

---
+++ cmake/compiler_options.cmake
@@ -0,0 +1,22 @@
+function(add_warnings_optimizations target_name)
+  if(MSVC)
+    target_compile_options(${target_name}
+      PRIVATE
+        /W4
+        /permissive-
+        $<$<CONFIG:RELEASE>:/O2 /Ob2>
+        $<$<CONFIG:MINSIZEREL>:/O1 /Ob1>
+        $<$<CONFIG:RELWITHDEBINFO>:/Zi /O2 /Ob1>
+        $<$<CONFIG:DEBUG>:/Zi /Ob0 /Od /RTC1>
+    )
+  else()
+    target_compile_options(${target_name}
+      PRIVATE
+        -Wall
+        -Wextra
+        -Wpedantic
+        $<$<CONFIG:RELEASE>:-O2>
+        $<$<CONFIG:DEBUG>:-O0 -g -p -pg>
+    )
+  endif()
+endfunction()
