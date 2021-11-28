e4cf74367b11c70e87346012bf75def7524bf536
krishpranav <krisna.pranav@gmail.com>
2021-11-28 15:47:27 +0530

    include: settings header

--- include/settings.h
+++ include/settings.h
@@ -1,35 +1,36 @@
-/* all the statics */
-
 #pragma once
 
-/**
- * @brief enable logging
- * 
- */
 #define KINGKONG_ENABLE_LOGGING
 
-/**
- * @brief log levels
- * 
- */
 #ifndef KINGKONG_LOG_LEVEL
 #define KINGKONG_LOG_LEVEL 1
 #endif
 
-/**
- * @brief static dir functions 
- */
-#ifndef KINGKONG_STATIC_DIR
-#define KONGKONG_STATIC_DIR "static/"
-#endif 
-
+#ifndef KINGKONG_STATIC_DIRECTORY
+#define KINGKONG_STATIC_DIRECTORY "static/"
+#endif
 #ifndef KINGKONG_STATIC_ENDPOINT
-#define KONGKONG_STATIC_ENDPOINT "/static/<path>/"
-#endif 
+#define KINGKONG_STATIC_ENDPOINT "/static/<path>"
+#endif
 
 #if defined(_MSVC_LANG) && _MSVC_LANG >= 201402L
 #define KINGKONG_CAN_USE_CPP14
 #endif
 #if __cplusplus >= 201402L
 #define KINGKONG_CAN_USE_CPP14
+#endif
+
+#if defined(_MSVC_LANG) && _MSVC_LANG >= 201703L
+#define KINGKONG_CAN_USE_CPP17
+#endif
+#if __cplusplus >= 201703L
+#define KINGKONG_CAN_USE_CPP17
+#endif
+
+#if defined(_MSC_VER)
+#if _MSC_VER < 1900
+#define KINGKONG_MSVC_WORKAROUND
+#define constexpr const
+#define noexcept throw()
+#endif
 #endif
