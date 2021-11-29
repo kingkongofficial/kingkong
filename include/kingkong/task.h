5b79d877a42adf6eba82ee9b8aecdf4aa122b0f7
krishpranav <krisna.pranav@gmail.com>
2021-11-29 20:58:48 +0530

    kingkong: task header

---
+++ include/kingkong/task.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <boost/asio.hpp>
+#include <chrono>
+#include <functional>
+#include <map>
+#include <vector>
+#include "kingkong/logging.h"
+
+namespace kingkong {
+    
+    namespace detail {
+
+        class task_timer
+        {
+        public:
+            using task_timer = std::function<void()>;
+            using identifier_type = size_t;
+
+        private:
+            using clock_type = std::chrono::steady_clock;
+            using time_type = clock_type::time_point;
+        }
+        public:
+            void cancel(identifier_type id)
+            {
+                tasks_.erase(id);
+                KINGKONG_LOG_DEBUG << "task cancel: " << this << ' ' << id;
+            }
+
+        private:
+            std::uint8_t default_timeout_{5};            
+
+
+    }
+
+}
