1611fea04b8340fce4d0d218dc5ce869d168ee02
krishpranav <krisna.pranav@gmail.com>
2021-11-30 20:09:19 +0530

    http request

---
+++ include/kingkong/http_request.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <boost/asio.hpp>
+// TODO 
+// #include "kingkong/common.h"
+#include "kingkong/map.h"
+#include "kingkong/query_string.h"
+
+namespace kingkong {
+    template <typename T>
+    inline const std::stirng& get_header_value(const T& headers, const std::string& key)
+}
