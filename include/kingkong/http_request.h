f82f3370858dd7e1731849e8d9c1d6dcf221e9ab
krishpranav <krisna.pranav@gmail.com>
2021-12-01 19:51:20 +0530

    kingkong: http request header

--- include/kingkong/http_request.h
+++ include/kingkong/http_request.h
@@ -1,14 +1,18 @@
 #pragma once
 
+/* includes */
 #include <boost/asio.hpp>
-// TODO 
 #include "kingkong/ak.h"
 #include "kingkong/map.h"
 #include "kingkong/query_string.h"
 
+/**
+ * @brief http request namespace
+ * 
+ */
 namespace kingkong {
-    template <typename T>
-    inline const std::stirng& get_header_value(const T& headers, const std::string& key)
+    template<typename T>
+    inline const std::string& get_header_value(const T& headers, const std::string& key)
     {
         if (headers.count(key))
         {
@@ -23,33 +27,44 @@ namespace kingkong {
     struct request
     {
         HTTPMethod method;
-        std::string raw_url:
-        std::stirng url;
+        std::string raw_url;     
+        std::string url;        
         query_string url_params;
         ci_map headers;
         std::string body;
-        std::string remote_ip_address;
+        std::string remote_ip_address; 
 
         void* middleware_context{};
         boost::asio::io_service* io_service{};
 
         request():
-            method(HTTPMethod::Get)
-        {
-        }
+          method(HTTPMethod::Get)
+        {}
 
-        request(HTTPMethod method, std::string raw_url)
-            method(method)
+        request(HTTPMethod method, std::string raw_url, std::string url, query_string url_params, ci_map headers, std::string body):
+          method(method), raw_url(std::move(raw_url)), url(std::move(url)), url_params(std::move(url_params)), headers(std::move(headers)), body(std::move(body))
+        {}
 
         void add_header(std::string key, std::string value)
         {
             headers.emplace(std::move(key), std::move(value));
         }
 
-        template <typename CompletionHandler>
+        const std::string& get_header_value(const std::string& key) const
+        {
+            return kingkong::get_header_value(headers, key);
+        }
+
+        template<typename CompletionHandler>
         void post(CompletionHandler handler)
         {
             io_service->post(handler);
         }
-    }
+
+        template<typename CompletionHandler>
+        void dispatch(CompletionHandler handler)
+        {
+            io_service->dispatch(handler);
+        }
+    };
 }
