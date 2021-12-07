678fca47d9b9f0b9576e0ffadff7a1decb769c90
krishpranav <krisna.pranav@gmail.com>
2021-12-07 19:36:16 +0530

    kingkong: json header

--- include/kingkong/json.h
+++ include/kingkong/json.h
@@ -1,9 +1,38 @@
 #pragma once 
 
+#include <string>
+#ifndef KINGKONG_JSON_USE_MAP
+#include <map>
+#else
+#include <unordered_map>
+#endif
+#include <iostream>
+#include <algorithm>
+#include <memory>
+#include <boost/lexical_cast.hpp>
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/operators.hpp>
+#include <vector>
+#include "kingkong/settings.h"
+#include "kingkong/returnable.h"
+
+#if defined(__GNUG__) || defined(__clang__)
+#define kingkong_json_likely(x) __builtin_expect(x, 1)
+#define kingkong_json_unlikely(x) __builtin_expect(x, 0)
+#else
+#define kingkong_json_likely(x) x
+#define kingkong_json_unlikely(x) x
+#endif
+
 namespace kingkong {
 
     namespace mustache {
         class template_t;
     }
-    
+
+    namespace json
+    {
+        inline void escape(const std::string& str, std::string& ret);
+    }
+
 }
