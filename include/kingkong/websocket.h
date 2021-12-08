f93620d6efd34c0871a49ce4b328e70796e76a9a
krishpranav <krisna.pranav@gmail.com>
2021-12-08 19:38:15 +0530

    kingkong: websocket header

--- include/kingkong/websocket.h
+++ include/kingkong/websocket.h
@@ -17,5 +17,22 @@ namespace kingkong {
             Mask,
             Payload,
         };
+
+        struct connection
+        {
+            virtual void send_binary(const std::string& msg) = 0;
+            virtual void send_text(const std::string& msg) = 0;
+            virtual void send_ping(const std::string& msg) = 0;
+            virtual void send_pong(const std::string& msg) = 0;
+            virtual void close(const std::string& msg = "quit") = 0;
+            virtual std::string get_remote_ip() = 0;
+            virtual ~connection() {}
+
+            void userdata(void* u) { userdata_ = u; }
+            void* userdata() { return userdata_; }
+
+        private:
+            void* userdata_;
+        };
     }
 }
