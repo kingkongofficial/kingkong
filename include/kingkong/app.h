a445cf23e9f5fef49765fddc59f60e4c7084e7ca
krishpranav <krisna.pranav@gmail.com>
2021-12-12 18:54:47 +0530

    kingkong: app header

--- include/kingkong/app.h
+++ include/kingkong/app.h
@@ -38,4 +38,60 @@ namespace kingkong {
     using ssl_context_t = boost::asio::ssl::context;
 #endif
 
+    template <typename... Middlewares>
+    class Kingkong
+    {
+    public:
+        using self_t = Kingkong
+        using server_t = Server<Kingkong, SocketAdaptor, Middlewares...>;
+#ifndef KINGKONG_ENABLE_SSL
+        using ssl_server_t = Server<Kingkong, SSLAdaptor, Middlewares...>;
+#endif
+
+    Kingkong() 
+    {}
+
+    template <typename Adaptor>
+    void handle_upgrade(const request& req, response& res, Adaptor&& adaptor)
+    {
+        router_.handle_upgrade(req, res, adaptor);
+    }
+
+    void handle(const request& req, response& res)
+    {
+        router_.handle(req, res);
+    }
+
+    DynamicRule& route_dynamic(std::string&& rule)
+    {
+        return router_.new_rule_dynamic(std::move(rule));
+    }
+
+    template <uint64_t Tag>
+#ifdef KINGKONG_GCC83_WORKAROUND
+        auto& route(std::string&& rule)
+#else
+        auto route(std::string&& rule)
+#endif
+#if defined KINGKONG_CAN_USE_CPP17 && !defined KINGKONG_GCC83_WORKAROUND
+          -> typename std::invoke_result<decltype(&Router::new_rule_tagged<Tag>), Router, std::string&&>::type
+#elif !defined KINGKONG_GCC83_WORKAROUND
+          -> typename std::result_of<decltype (&Router::new_rule_tagged<Tag>)(Router, std::string&&)>::type
+#endif
+        {
+            return router_.new_rule_tagged<Tag>(std::move(rule));
+        }
+
+        CatchallRule& catchall_route()
+        {
+            return router_.catchall_rule();
+        }
+
+        self_t& signal_clear()
+        {
+            signals_.clear();
+            return *this;
+        }
+
+    }
 }
