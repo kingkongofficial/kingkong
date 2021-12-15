6ec44d4f0fc7f847b93fa4632c854fecdbd68efb
krishpranav <krisna.pranav@gmail.com>
2021-12-15 20:27:42 +0530

    examples: helloworld

--- examples/helloworld.cpp
+++ examples/helloworld.cpp
@@ -1,1 +1,12 @@
 #include "kingkong.h"
+
+int main() {
+    kingkong::SimpleApp app;
+
+    KINGKONG_ROUTE(app, "/")
+    ([] () {
+        return "Hello World";
+    });
+
+    app.port(10000).run();
+}
