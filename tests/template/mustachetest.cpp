96a963e712503fcaf1570f2c339b83d89c638bf5
krishpranav <krisna.pranav@gmail.com>
2021-12-19 12:32:23 +0530

    tests: template mustachetest

---
+++ tests/template/mustachetest.cpp
@@ -0,0 +1,34 @@
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <string>
+
+#include "kingkong/json.h"
+#include "kingkong/mustache.h"
+
+using namespace std;
+using namespace kingkong;
+using namespace kingkong::mustache;
+
+string read_all(const string& filename)
+{
+    ifstream is(filename);
+    return {istreambuf_iterator<char>(is), istreambuf_iterator<char>()};
+}
+
+int main()
+{
+    auto data = json::load(read_all("data"));
+    auto templ = compile(read_all("template"));
+    auto partials = json::load(read_all("partials"));
+    set_loader([&](std::string name) -> std::string {
+        if (partials.count(name))
+        {
+            return partials[name].s();
+        }
+        return "";
+    });
+    context ctx(data);
+    cout << templ.render(ctx);
+    return 0;
+}
