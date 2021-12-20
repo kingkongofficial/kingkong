97036ae5aec36c5b05df29da6220c9046371d88d
krishpranav <krisna.pranav@gmail.com>
2021-12-20 20:07:27 +0530

    cargo

--- docs/learnkingkong.md
+++
@@ -1,8 +0,0 @@
-## Learn King Kong 
-
-- this docs will help you how to use kingkong framework
-
-- Basic rest api:
-```cpp
-// soon :)
-```
--- include/kingkong/version.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace kingkong {
-    /**
-     * @brief version of kinkong
-     * 
-     */
-    constexpr const char version[] = "dev-1.0";
-}
