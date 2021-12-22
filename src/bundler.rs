4ef1608ef2bf791555ea9c46a3dff15080fc00b4
krishpranav <krisna.pranav@gmail.com>
2021-12-22 17:42:11 +0530

    bundler: walk + files in dir

--- src/bundler.rs
+++ src/bundler.rs
@@ -9,3 +9,17 @@ use {
         path::{Path, PathBuf},
     },
 };
+
+#[doc(hidden)]
+#[allow(dead_code)]
+pub fn walk(dir: &str) -> std::vec::IntoIter<PathBuf> {
+    if let Ok(files) = files_in_dir(dir) {
+        files.into_iter()
+    } else {
+        vec![].into_iter()
+    }
+}
+
+fn files_in_dir(path: &str) -> Result<Vec<PathBuf>> {
+    
+}
