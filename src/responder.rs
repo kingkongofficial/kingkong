268d5d5bef5d7e06a3336c8f23aa6b90fd9e9bde
krishpranav <krisna.pranav@gmail.com>
2021-12-22 19:32:37 +0530

    responder: impl

--- src/responder.rs
+++ src/responder.rs
@@ -1,1 +1,17 @@
 use crate::Response;
+
+pub trait Responder {
+    fn to_response(self) -> Response;
+}
+
+impl Responsed for Response {
+    fn to_response(self) -> Response {
+        self
+    }
+}
+
+impl Responder for &str {
+    fn to_response(self) -> Response {
+        Response::from(self)
+    }
+}
