a3131fcd632852e13d873124689fcb0a538549e7
krishpranav <krisna.pranav@gmail.com>
2021-12-22 18:54:46 +0530

    request: from buf + is_empty + new

--- src/request.rs
+++ src/request.rs
@@ -6,12 +6,22 @@ use {
 #[cfg(feature = "cookies")]
 use cookie2::Cookie;
 
-impl Scan {
+impl Span {
     pub fn new() -> Span {
         Span::default()
     }
 
     pub fn is_empty(&self) -> bool {
         self.0 == 0 && self.1 == 0
+    }
+
+    pub fn from_buf<'buf>(&self, buf: &'buf [u8]) -> &'buf str {
+        if self.is_empty() {
+            ""
+        } else if self.1 >= self.0 && self.1 <= buf.len() {
+            str::from_utf8(&buf[self.0..self.1]).unwrap_or("?")
+        } else {
+            "?"
+        }
     }
 }
