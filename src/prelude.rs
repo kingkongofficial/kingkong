11418d9fe6f0bb3f84344bd0b3278ff1f419b08e
krishpranav <krisna.pranav@gmail.com>
2021-12-22 19:05:28 +0530

    prelude: crate + feature

---
+++ src/prelude.rs
@@ -0,0 +1,9 @@
+pub use crate::{
+    asset, asset_dir, method::Method, request::Request, responder::Responder, response::Response,
+    router::Router, routes, run,
+};
+
+pub use crate::use_state;
+
+#[cfg(feature = "hatter")]
+pub use hatter;
