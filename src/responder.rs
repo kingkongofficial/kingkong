0f092c5b5383bb16d363481368d35e32b1de7d3e
krishpranav <krisna.pranav@gmail.com>
2021-12-22 19:33:04 +0530

    responder: impl + fn

--- src/responder.rs
+++ src/responder.rs
@@ -4,7 +4,7 @@ pub trait Responder {
     fn to_response(self) -> Response;
 }
 
-impl Responsed for Response {
+impl Responder for Response {
     fn to_response(self) -> Response {
         self
     }
@@ -13,5 +13,47 @@ impl Responsed for Response {
 impl Responder for &str {
     fn to_response(self) -> Response {
         Response::from(self)
+    }
+}
+
+impl Responder for String {
+    fn to_response(self) -> Response {
+        Response::from(self)
+    }
+}
+
+impl Responder for usize {
+    fn to_response(self) -> Response {
+        Response::from(self)
+    }
+}
+
+impl<T: Responder, E: std::error::Error> Responder for Result<T, E> {
+    fn to_response(self) -> Response {
+        match self {
+            Err(e) => Response::from_error(e),
+            Ok(s) => s.to_response(),
+        }
+    }
+}
+
+impl<T: Responder> Responder for Option<T> {
+    fn to_response(self) -> Response {
+        match self {
+            None => Response::from_code(404),
+            Some(s) => s.to_response(),
+        }
+    }
+}
+
+impl Responder for crate::Error {
+    fn to_response(self) -> Response {
+        Response::from_error(self)
+    }
+}
+
+impl Responder for () {
+    fn to_response(self) -> Response {
+        Response::default()
     }
 }
