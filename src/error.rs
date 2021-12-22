a684d75c8c421c4c269d4d9b26b9613e19f7a52a
krishpranav <krisna.pranav@gmail.com>
2021-12-22 18:07:49 +0530

    error: impl

--- src/error.rs
+++ src/error.rs
@@ -13,3 +13,28 @@ pub enum Error {
     IO(io::Error),
     Other(String),
 }
+
+impl From<Error> for io::Error {
+    fn from(err: Error) -> io::Error {
+        io::Error::new(io::ErrorKind::Other, err.to_string())
+    }
+}
+
+impl From<Error> for io::Error {
+    fn from(err: io::Error) -> Error {
+        Error::IO(err)
+    }
+}
+
+impl ErrorTrait for Error {
+    fn source(&self) -> Option<&(dyn ErrorTrait + 'static)> {
+        match self {
+            Error::IO(source) => Some(source),
+            _ => None, 
+        }
+    }
+}
+
+impl fmt::Display for Error {
+
+}
