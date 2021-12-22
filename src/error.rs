1b52d78348e9fe8137deac777123592560913273
krishpranav <krisna.pranav@gmail.com>
2021-12-22 18:08:36 +0530

    error: update error codes and some funcs..

--- src/error.rs
+++ src/error.rs
@@ -20,7 +20,7 @@ impl From<Error> for io::Error {
     }
 }
 
-impl From<Error> for io::Error {
+impl From<io::Error> for Error {
     fn from(err: io::Error) -> Error {
         Error::IO(err)
     }
@@ -30,11 +30,55 @@ impl ErrorTrait for Error {
     fn source(&self) -> Option<&(dyn ErrorTrait + 'static)> {
         match self {
             Error::IO(source) => Some(source),
-            _ => None, 
+            _ => None,
         }
     }
 }
 
 impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Error::UnknownHTTPMethod(reason) => &reason,
+                Error::ConnectionClosed => "Connection Closed By Client",
+                Error::ParseVersion => "Error Parsing HTTP Version",
+                Error::ExpectedCRLF => "Expected CRLF in HTTP Request",
+                Error::ParseHeaderName => "Error Parsing HTTP Header name",
+                Error::ParseHeaderValue => "Error Parsing HTTP Header value",
+                Error::ParseError => "Error Parsing HTTP Request",
+                Error::AssetNotFound(..) => "Can't Find Asset",
+                Error::IO(..) => "io::Error While Parsing HTTP Request",
+                Error::Other(reason) => &reason,
+            }
+        )
+    }
+}
 
+impl PartialEq for Error {
+    fn eq(&self, other: &Self) -> bool {
+        use Error::*;
+        match self {
+            IO(_) => false,
+            AssetNotFound(s) => match other {
+                AssetNotFound(o) => s == o,
+                _ => false,
+            },
+            UnknownHTTPMethod(s) => match other {
+                UnknownHTTPMethod(o) => s == o,
+                _ => false,
+            },
+            Other(s) => match other {
+                Other(o) => s == o,
+                _ => false,
+            },
+            ConnectionClosed => matches!(other, ConnectionClosed),
+            ParseVersion => matches!(other, ParseVersion),
+            ExpectedCRLF => matches!(other, ExpectedCRLF),
+            ParseHeaderName => matches!(other, ParseHeaderName),
+            ParseHeaderValue => matches!(other, ParseHeaderValue),
+            ParseError => matches!(other, ParseError),
+        }
+    }
 }
