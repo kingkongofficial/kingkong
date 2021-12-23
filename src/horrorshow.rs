5d4944b4e4f755c467aaa615721899396645663e
krishpranav <krisna.pranav@gmail.com>
2021-12-23 16:20:33 +0530

    horrorshow

--- src/horrorshow.rs
+++ src/horrorshow.rs
@@ -3,3 +3,13 @@ use {
     horrorshow::{FnRenderer, TemplateBuffer},
     std::fmt,
 };
+
+impl<F> Responder for FnRenderer<F>
+where
+    FnRenderer<F>: fmt::Display,
+    F: FnOnce(&mut TemplateBuffer<'_>),
+{
+    fn to_response(self) -> Response {
+        Response::from_body(self.to_string())
+    }
+}
