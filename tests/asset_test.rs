2d8f8fa66ecf69843124d53f4d06ebcd557b17e5
krishpranav <krisna.pranav@gmail.com>
2021-12-23 16:51:04 +0530

    tests: asset test

---
+++ tests/asset_test.rs
@@ -0,0 +1,28 @@
+use kingkong::{self, asset};
+
+/**
+ * entry point
+ */
+#[test]
+fn asset_tests() {
+    asset_exists();
+    etag_test();
+    normalize_path_test();
+    to_string_test();
+}
+
+fn asset_exists() {
+
+}
+
+fn etag_test() {
+
+}
+
+fn normalize_path_test() {
+
+}
+
+fn to_string_test() {
+    
+}
