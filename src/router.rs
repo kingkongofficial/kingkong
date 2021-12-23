ad57a974cb316df5bdae5c4bb5ca2ea3d9dfc5e1
krishpranav <krisna.pranav@gmail.com>
2021-12-23 11:08:33 +0530

    router: pattern to vec

--- src/router.rs
+++ src/router.rs
@@ -14,5 +14,28 @@ pub struct Router {
 }
 
 impl Router {
-    
+    pub fn new() -> Router {
+        Router {
+            routes: HashMap::new(),
+        }
+    }
+
+    pub fn action_for(&self, req: &mut Request) -> Option<&Action> {
+
+    }
+
+    fn pattern_to_vec(pattern: &str) -> Pattern {
+        pattern
+            .trim_matches('/')
+            .split('/')
+            .flat_map(|s| {
+                if let Some(idx) = s.find('.') {
+                    vec![s[..idx].to_string(), s[idx..].to_string()]
+                } else {
+                    vec![s.to_string()]
+                }
+            })
+            .collect::<Vec<_>>()
+    }
+
 }
