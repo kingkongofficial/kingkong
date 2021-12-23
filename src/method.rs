359e1946ba5628473759d8456ad18783ca701d5f
krishpranav <krisna.pranav@gmail.com>
2021-12-23 11:01:11 +0530

    method: method errors + str

--- src/method.rs
+++ src/method.rs
@@ -14,4 +14,18 @@ pub enum Method {
 
 impl std::str::FromStr for Method {
     type Err = Error;
+    
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "GET" => Method::GET,
+            "HEAD" => Method::HEAD,
+            "POST" => Method::POST,
+            "PUT" => Method::PUT,
+            "DELETE" => Method::DELETE,
+            "PATCH" => Method::PATCH,
+            "OPTIONS" => Method::OPTIONS,
+            "TRACE" => Method::TRACE,
+            _ => return Err(Error::UnknownHTTPMethod(s.into())),
+        })
+    }
 }
