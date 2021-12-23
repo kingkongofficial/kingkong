83306f9c715ff7e5e467b4b733d54fc128f76855
krishpranav <krisna.pranav@gmail.com>
2021-12-23 16:26:42 +0530

    libs update

--- src/lib.rs
+++ src/lib.rs
@@ -16,3 +16,31 @@ mod response;
 mod router;
 mod server;
 
+#[doc(hidden)]
+pub mod bundler;
+#[doc(hidden)]
+pub mod http_parser;
+#[doc(hidden)]
+pub mod util;
+
+#[doc(hidden)]
+pub mod storage;
+
+#[cfg(feature = "horror")]
+#[doc(hidden)]
+pub mod horrorshow;
+
+pub use {
+    bundler::bundle_assets, cache::TypeCache, error::Error, method::Method, request::Request,
+    responder::Responder, response::Response, router::Router, server::run,
+};
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+pub static mut ASSET_DIR: Option<String> = None;
+
+pub static mut BUNDLED_ASSETS: Option<std::collections::HashMap<String, &'static [u8]>> = None;
+
+pub const BUILD_DATE: &str = env!("BUILD_DATE");
+
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
